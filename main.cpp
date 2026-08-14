73a4d0b1575e98d3004cef69354a55b84dd2a564
ricci <guilhermericci1445@gmail.com>
2026-08-14 17:44:10 -0300

    main function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <libcryptosec/MessageDigest.h>
 
+#include "include/agreement/multi_signature.hpp"
+#include "include/file_io/file_handler.hpp"
+#include "include/operator/operator.hpp"
+#include "include/paths/paths.hpp"
+
 int main(int argc, char **argv) {
-	printf("Hello There!\n");
+
+    // função para rodar o programa
+
+    printf("Hello There!\n");
 	
 	MessageDigest::loadMessageDigestAlgorithms();
 	
