6fd5e450a6dd0f89dfc4c5bcd45f9b5201cbde26
ricci <guilhermericci1445@gmail.com>
2026-08-16 04:25:54 -0300

    main function impl

--- main.cpp
+++ main.cpp
@@ -6,13 +6,23 @@
 #include "include/operator/operator.hpp"
 #include "include/paths/paths.hpp"
 
+using namespace sgc;
+using namespace op;
+
+const char* PDF_PATH = "/home/gui/Documents/code/pam/testes/teste.pdf";
+
 int main(int argc, char **argv) {
 
     // função para rodar o programa
-
-    printf("Hello There!\n");
-	
 	MessageDigest::loadMessageDigestAlgorithms();
 	
+    // read pdf file
+    FileHandle fileHandler;
+    ByteArray document = fileHandler.read_file(PDF_PATH);
+
+    // usuario cria operadores com seus respectivo atributos
+
+    // montar os operator para o cliente
+
 	return 0;
 }
